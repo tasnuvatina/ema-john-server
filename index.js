ad7760f4198382398c17af2dc2b0dca8500203c6
Tasnuva Rahman <tasnuvarahman1996@gmail.com>
2021-03-28 08:41:20 -0700

    port added and nodemon fixed

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(bodyParser.json());
 app.use(cors());
 const port = 4000;
 
+app.get('/',(req,res)=>{
+  res.send('working..............')
+})
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
